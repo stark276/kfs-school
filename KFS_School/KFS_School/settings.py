ef46176f65ede411205ddd88751de46c4db32425
Nicole Abadie <nic4096@gmail.com>
2020-08-27 12:24:11 -0700

    dotenv added

--- KFS_School/KFS_School/settings.py
+++ KFS_School/KFS_School/settings.py
@@ -11,16 +11,21 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Add .env variables anywhere before SECRET_KEY
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$9wsdodw0&@-t0kjz4=&ufxi_j45_07u8@-8l9u4v#6-%_%hpy'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- README.md
+++ README.md
@@ -3,9 +3,29 @@
 
 ## Getting Started
 
-1. ```git clone https://github.com/AcidicNic/KFS-School.git```
-1. ```python3 -m venv .env```
-1. ```source .env/bin/activate```
-1. ```pip3 install -r requirements.txt```
-1. ```cd KFS_School```
-1. ```python manage.py runserver```
+1. ```bash
+$ git clone https://github.com/AcidicNic/KFS-School.git
+```
+1. ```bash
+$ python3 -m venv .venv
+```
+1. ```bash
+$ source .venv/bin/activate
+```
+1. ```bash
+$ pip3 install -r requirements.txt
+```
+1. ```bash
+$ cd KFS_School
+```
+1. ```bash
+$ touch .env
+```
+    - Inside .env define your [secret key](https://miniwebtool.com/django-secret-key-generator/) like this:
+    - ```bash
+    SECRET_KEY=some_random_key
+    ```
+
+1. ```bash
+$ python3 manage.py runserver
+```
